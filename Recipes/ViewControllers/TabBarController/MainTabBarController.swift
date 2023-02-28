e2c9f2589c1de92d825cbfad0867736b4d259d79
Koposov Seryozha <koposov.serezha@gmail.com>
2023-02-28 09:00:27 +0300

    fix tabBar

--- Recipes/ViewControllers/TabBarController/MainTabBarController.swift
+++ Recipes/ViewControllers/TabBarController/MainTabBarController.swift
@@ -25,9 +25,8 @@ class MainTabBarController: UITabBarController {
     }
 
     func createNavController(viewController: UIViewController, itemName: String, itemImage: String) -> UINavigationController {
-        let item = UITabBarItem(title: itemName, image: UIImage(systemName: itemImage)?.withAlignmentRectInsets(.init(top: 10, left: 0, bottom: 0, right: 0)), tag: 0)
+        let item = UITabBarItem(title: itemName, image: UIImage(systemName: itemImage), tag: 0)
 
-        item.titlePositionAdjustment = .init(horizontal: 0, vertical: 10)
 
         let navigationController = UINavigationController(rootViewController: viewController)
         navigationController.tabBarItem = item
