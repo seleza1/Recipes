ceab4437d40aa6f7eba92f1deef7cd35fe9ec9e9
Koposov Seryozha <koposov.serezha@gmail.com>
2023-03-02 18:42:32 +0300

    edits

--- Recipes/Applications/SceneDelegate.swift
+++ Recipes/Applications/SceneDelegate.swift
@@ -17,18 +17,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.makeKeyAndVisible()
         window?.rootViewController = LoginViewController()
     }
-    func sceneDidDisconnect(_ scene: UIScene) {
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-    }
 }
